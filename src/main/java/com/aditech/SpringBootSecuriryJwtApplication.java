c7806769dad9f8ad596237ea8237c09e6a44529a
Adityatech16 <gajanangaikawad15@gmail.com>
2024-03-03 16:05:23 +0530

    spring jwt

---
+++ src/main/java/com/aditech/SpringBootSecuriryJwtApplication.java
@@ -0,0 +1,13 @@
+package com.aditech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuriryJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecuriryJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditech/SpringBootSecuriryJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecuriryJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
